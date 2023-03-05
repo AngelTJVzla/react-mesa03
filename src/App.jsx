49dea4821b2a06aab4c8933435399528bae7a83f
Angel A Ontiveros P <89170175+AngelTJVzla@users.noreply.github.com>
2023-03-05 16:26:06 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+function App() {
+  return (
+    <div className="div">
+      <header>
+        <p id="header">Mesa de Trabalho 3</p>
+      </header>
+      <div>
+        <p id="titulo">Lorem ipsum</p>
+      </div>
+      <main className="main">
+        <p id="text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae
+          lacus ut ligula mattis mattis. Duis lectus libero, efficitur quis
+          posuere eu, volutpat id leo. Cras sed diam ac tortor pulvinar iaculis
+          at id velit. Maecenas eget tellus ut nunc laoreet dictum. Quisque arcu
+          mauris, accumsan sit amet suscipit vitae, sollicitudin at leo.
+        </p>
+        <p id="text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae
+          lacus ut ligula mattis mattis. Duis lectus libero, efficitur quis
+          posuere eu, volutpat id leo. Cras sed diam ac tortor pulvinar iaculis
+          at id velit. Maecenas eget tellus ut nunc laoreet dictum. Quisque arcu
+          mauris, accumsan sit amet suscipit vitae, sollicitudin at leo.
+        </p>
+        <p id="text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae
+          lacus ut ligula mattis mattis. Duis lectus libero, efficitur quis
+          posuere eu, volutpat id leo. Cras sed diam ac tortor pulvinar iaculis
+          at id velit. Maecenas eget tellus ut nunc laoreet dictum. Quisque arcu
+          mauris, accumsan sit amet suscipit vitae, sollicitudin at leo.
+        </p>
+        <p id="text" className="equipe">
+          Equipe: Alícia, Josemar, Angel, Mahiny, Ricardo{" "}
+        </p>
+      </main>
+      <div>
+        <footer>
+          <p id="footer">Todos os direitos reservados 2022</p>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
